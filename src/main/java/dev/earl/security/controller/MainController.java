b6cf266995c398e51c12a1bca58201f9a5850c15
Earl Padron <earlpadron24@gmail.com>
2024-04-18 13:01:51 -0700

    finished ch10_cors

--- src/main/java/dev/earl/security/controller/MainController.java
+++ src/main/java/dev/earl/security/controller/MainController.java
@@ -1,13 +1,27 @@
 package dev.earl.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 @Controller
 public class MainController {
+    private final Logger logger = Logger.getLogger(MainController.class.getName());
 
     @GetMapping("/main")
     public String main(){
         return "main.html";
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    @CrossOrigin("http://localhost:8080")
+    public String test(){
+        logger.info("Test method called");
+        return "HELLO from MainController";
     }
 }
